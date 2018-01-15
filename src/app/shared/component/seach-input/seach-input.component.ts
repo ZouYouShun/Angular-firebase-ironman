0953f9af909c9e994e69e7591e795bf0b1496930
AlanZou <e936106@gmail.com>
2018-01-15 19:54:53 +0800

    add debonceTime

--- src/app/shared/component/seach-input/seach-input.component.ts
+++ src/app/shared/component/seach-input/seach-input.component.ts
@@ -1,20 +1,40 @@
-import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/debounceTime';
+
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { StringHandler } from '@shared/ts/data/string.handler';
+import { Observable } from 'rxjs/Observable';
+import { AutoDestroy } from '@shared/ts/auto.destroy';
 
 @Component({
   selector: 'app-seach-input',
   templateUrl: './seach-input.component.html',
   styleUrls: ['./seach-input.component.scss']
 })
-export class SeachInputComponent implements OnInit {
+export class SeachInputComponent extends AutoDestroy implements OnInit, AfterViewInit {
   @Output() submit = new EventEmitter();
   @Output() clear = new EventEmitter();
   @ViewChild('input', { read: ElementRef }) input: ElementRef;
 
   active = false;
-  constructor() { }
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    Observable.fromEvent(this.input.nativeElement, 'keyup')
+      .debounceTime(500)
+      .do((e: KeyboardEvent) => {
+        if (e.keyCode !== 13) {
+          console.log(e);
+          this.onSubmit();
+        }
+      })
+      .takeUntil(this._destroy$)
+      .subscribe();
   }
 
   search(state = true) {
